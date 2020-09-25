dc2bedffb171e61f2789c43ef6af07f2d1d6f2e0
Florian Mülhauser <florian.mulhauser@arcanite.ch>
2020-09-25 14:10:28 +0200

    add lombok and Homepae model

---
+++ src/main/java/Model/HomePage.java
@@ -0,0 +1,28 @@
+package Model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
+
+import java.util.ArrayList;
+
+
+@Setter
+@Getter
+@Builder
+public class HomePage {
+    private Boolean isConnected;
+    private String description;
+    private String rules;
+    private ArrayList<Integer> statistics;
+
+    public HomePage(Boolean isConnected, String description, String rules, ArrayList<Integer> statistics){
+        this.description = description;
+        this.rules = rules;
+        this.statistics = statistics;
+        this.isConnected = isConnected;
+    }
+
+}
+
