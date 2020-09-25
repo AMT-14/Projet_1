b9ba3aa4e0d6b3d60b65b548ec94be9405ebf179
Basile <basile.botebol@gmail.com>
2020-09-25 15:20:19 +0200

    fixed classes

--- src/main/java/Model/Question.java
+++ src/main/java/Model/Question.java
@@ -1,28 +1,15 @@
 package Model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@Builder
 public class Question {
     private User author;
     private String body;
-
-    public Question(User author, String question) {
-        this.author = author;
-        this.body = question;
-    }
-
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    public String getQuestion() {
-        return body;
-    }
-
-    public void setQuestion(String question) {
-        this.body = question;
-    }
+    
 }
