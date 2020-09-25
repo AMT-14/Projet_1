736faf3a3f9b89c8caf2e1d5a51f9bba645576b0
Basile <basile.botebol@gmail.com>
2020-09-25 13:00:44 +0200

    early versions of User and Question

--- src/main/java/Model/Question.java
+++ src/main/java/Model/Question.java
@@ -1,6 +1,28 @@
 package Model;
 
 public class Question {
-    private
+    private User author;
+    private String question;
 
+    public Question(User author, String question) {
+        this.author = author;
+        this.question = question;
+    }
+
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
 }
