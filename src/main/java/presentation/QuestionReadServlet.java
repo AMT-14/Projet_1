30ad0bda22c1d1544b99dedeb956e25d7fa68eca
Basile <basile.botebol@gmail.com>
2020-09-26 09:30:17 +0200

    working dockerization and build.sh script

--- src/main/java/presentation/QuestionReadServlet.java
+++ src/main/java/presentation/QuestionReadServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet; import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest; import javax.servlet.http.HttpServletResponse; import java.io.IOException;
 
-@WebServlet(name = "QuestionRead", urlPatterns = "/questionread")
+@WebServlet(name = "QuestionReadServlet", urlPatterns = "/questionread")
 public class QuestionReadServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
