9b40de0e537793f307c15a23ac5d759eda314b31
Maxime Dupont <maxime.dupont@heig-vd.ch>
2020-10-02 14:19:03 +0200

    adapted joke use case

--- src/main/java/ch/heigvd/amt/domain/question/IQuestionRepository.java
+++ src/main/java/ch/heigvd/amt/domain/question/IQuestionRepository.java
@@ -1,10 +1,11 @@
 package ch.heigvd.amt.domain.question;
 
 
+import ch.heigvd.amt.application.question.QuestionsQuery;
 import ch.heigvd.amt.domain.IRepository;
 
 import java.util.Collection;
 
 public interface  IQuestionRepository extends IRepository<Question, QuestionId> {
-    public Collection<Question> find(QuestionQuery query);
+    public Collection<Question> find(QuestionsQuery query);
 }
--- src/main/java/ch/heigvd/amt/infrastructure/persistence/memory/InMemoryQuestionRepository.java
+++ src/main/java/ch/heigvd/amt/infrastructure/persistence/memory/InMemoryQuestionRepository.java
@@ -1,5 +1,6 @@
 package ch.heigvd.amt.infrastructure.persistence.memory;
 
+import ch.heigvd.amt.application.question.QuestionsQuery;
 import ch.heigvd.amt.domain.question.IQuestionRepository;
 import ch.heigvd.amt.domain.question.Question;
 import ch.heigvd.amt.domain.question.QuestionId;
@@ -42,7 +43,7 @@ public class InMemoryQuestionRepository implements IQuestionRepository{
     }
 
     @Override
-    public Collection<Question> find(QuestionQuery query){
+    public Collection<Question> find(QuestionsQuery query){
         if(query != null){
             return findAll().stream()
                     .filter(question -> question.getQuestionType() != QuestionType.NOT_SAFE_FOR_WORK)
