c0fad9e1a65f856bc3c58e6fed8008611eda8c36
Florian Mülhauser <florian.mulhauser@heig-vd.ch>
2020-10-29 21:12:38 +0100

    jbdc implemented for votes. Still need findAll() and FindById() but for later

--- src/main/java/ch/heigvd/amt/infrastructure/persistence/jdbc/JdbcVoteRepository.java
+++ src/main/java/ch/heigvd/amt/infrastructure/persistence/jdbc/JdbcVoteRepository.java
@@ -1,4 +1,226 @@
 package ch.heigvd.amt.infrastructure.persistence.jdbc;
 
-public class JdbcVoteRepository {
+import ch.heigvd.amt.domain.Id;
+import ch.heigvd.amt.domain.TextType;
+import ch.heigvd.amt.domain.question.QuestionId;
+import ch.heigvd.amt.domain.user.IUserRepository;
+import ch.heigvd.amt.domain.user.User;
+import ch.heigvd.amt.domain.user.UserId;
+import ch.heigvd.amt.domain.vote.IVoteRepository;
+import ch.heigvd.amt.domain.vote.Vote;
+import ch.heigvd.amt.domain.vote.VoteId;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@ApplicationScoped
+@Named("JdbcUserRepository")
+public class JdbcVoteRepository implements IVoteRepository {
+
+    @Resource (lookup ="jdbc/DBProjet")
+    DataSource dataSource;
+
+    public JdbcVoteRepository() {
+    }
+
+    public JdbcVoteRepository(DataSource dataSource) { this.dataSource = dataSource; }
+
+    private Vote resultSetVoted(ResultSet rs, Id objectVoted) throws SQLException{
+        return Vote.builder()
+                .id(new VoteId(rs.getString("id")))
+                .voter(new UserId(rs.getString("idUser")))
+                .objectVoted(objectVoted)
+                .value(rs.getBoolean("value") ? Vote.VoteValue.UP : Vote.VoteValue.DOWN)
+                .build();
+    }
+
+    @Override
+    public int totalVotes(Id id, TextType textType) {
+        int total = 0;
+
+        try {
+            Connection conn = dataSource.getConnection();
+
+            String query;
+            if(textType.equals(TextType.QUESTION)){
+                query = "SELECT totalVotes FROM question WHERE question_id LIKE ?";
+            } else if(textType.equals(TextType.ANSWER)) {
+                query = "SELECT totalVotes FROM answer WHERE question_id LIKE ?";
+            } else {
+                query = "error";
+            }
+
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, id.asString());
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()){
+                total = rs.getInt(1);
+            }
+
+            ps.close();
+            conn.close();
+
+        } catch (SQLException throwables) {
+            Logger.getLogger(JdbcVoteRepository.class.getName()).log(Level.SEVERE, null, throwables);
+        }
+        return total;
+    }
+
+    @Override
+    public Optional<Vote> getVote(Id id, UserId uid, TextType textType) {
+        Optional<Vote> vote = Optional.empty();
+        try {
+            Connection conn = dataSource.getConnection();
+
+            String query="SELECT * FROM vote WHERE id LIKE ? AND author_id like ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, id.asString());
+            ps.setString(2, uid.asString());
+            ResultSet rs = ps.executeQuery();
+
+            Collection<Vote> votes = new LinkedList<>();
+            if(textType.equals(TextType.QUESTION)){
+                while(rs.next()){
+                    votes.add(resultSetVoted(rs, uid));
+                }
+            } else if(textType.equals(TextType.ANSWER)) {
+                while(rs.next()){
+//                    votes.add(resultSetVoted(rs, new AnswerId(rs.getString("author_id"))));
+                }
+            }
+
+            if(votes.size() == 1){
+                vote = votes.stream().findFirst();
+            } else if(votes.size() > 1){
+                throw new SQLDataException("2 things with same ID");
+            }
+
+            ps.close();
+            conn.close();
+
+        } catch (SQLException throwables) {
+            Logger.getLogger(JdbcVoteRepository.class.getName()).log(Level.SEVERE, null, throwables);
+        }
+        return vote;
+    }
+
+    @Override
+    public Optional<Vote> getVote(VoteId id, TextType textType) {
+        Optional<Vote> vote = Optional.empty();
+        try {
+            Connection conn = dataSource.getConnection();
+
+            String query="SELECT * FROM vote WHERE id LIKE ? ";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, id.asString());
+            ResultSet rs = ps.executeQuery();
+
+            Collection<Vote> votes = new LinkedList<>();
+            if(textType.equals(TextType.QUESTION)){
+                while(rs.next()){
+                    votes.add(resultSetVoted(rs, new QuestionId(rs.getString("author_id"))));
+                }
+            } else if(textType.equals(TextType.ANSWER)) {
+                while(rs.next()){
+//                    votes.add(resultSetVoted(rs, new AnswerId(rs.getString("author_id"))));
+                }
+            }
+
+            if(votes.size() == 1){
+                vote = votes.stream().findFirst();
+            } else if(votes.size() > 1){
+                throw new SQLDataException("2 things with same ID");
+            }
+
+            ps.close();
+            conn.close();
+
+        } catch (SQLException throwables) {
+            Logger.getLogger(JdbcVoteRepository.class.getName()).log(Level.SEVERE, null, throwables);
+        }
+        return vote;
+    }
+
+    @Override
+    public void save(Vote vote) {
+
+        try {
+
+            Connection conn = dataSource.getConnection();
+            String query = "INSERT INTO vote (vote_id, objectVoted, voter, value)"
+                    + " VALUES (?, ?, ?, ?)";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, vote.getId().asString());
+            ps.setString(2, vote.getObjectVoted().asString());
+            ps.setString(3, vote.getVoter().asString());
+            ps.setBoolean(4, vote.getValue() == Vote.VoteValue.UP); // pour booleaniser
+
+            ps.executeUpdate();
+
+            ps.close();
+            conn.close();
+
+        } catch (SQLException throwables) {
+            Logger.getLogger(JdbcUserRepository.class.getName()).log(Level.SEVERE, null, throwables);
+        }
+    }
+
+    @Override
+    public void remove(VoteId voteId) {
+
+        try {
+            Connection conn = dataSource.getConnection();
+
+            String query = "DELETE FROM vote WHERE vote_id LIKE ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, voteId.asString());
+            ps.executeUpdate();
+
+            ps.close();
+            conn.close();
+
+        } catch (SQLException throwables) {
+            Logger.getLogger(JdbcUserRepository.class.getName()).log(Level.SEVERE, null, throwables);
+        }
+    }
+
+    @Override
+    public void update(Vote vote) {
+
+        try {
+            Connection conn = dataSource.getConnection();
+
+            String query = "UPDATE FROM vote SET value LIKE ? WHERE vote_id LIKE ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setBoolean(1, vote.getValue() == Vote.VoteValue.UP);
+            ps.setString(2, vote.getId().asString());
+            ps.executeUpdate();
+
+            ps.close();
+            conn.close();
+
+        } catch (SQLException throwables) {
+            Logger.getLogger(JdbcUserRepository.class.getName()).log(Level.SEVERE, null, throwables);
+        }
+    }
+
+    @Override
+    public Optional<Vote> findById(VoteId voteId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Collection<Vote> findAll() {
+        return null;
+    }
 }
