14175a361f8213cb3c73606adc6a00678d9c86bb
ClaudeAlves <claudeuw@hotmail.com>
2020-10-30 09:05:43 +0100

    changes injection name

--- src/main/java/ch/heigvd/amt/infrastructure/persistence/jdbc/JdbcVoteRepository.java
+++ src/main/java/ch/heigvd/amt/infrastructure/persistence/jdbc/JdbcVoteRepository.java
@@ -23,7 +23,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ApplicationScoped
-@Named("JdbcUserRepository")
+@Named("JdbcVoteRepository")
 public class JdbcVoteRepository implements IVoteRepository {
 
     @Resource (lookup ="jdbc/DBProjet")
