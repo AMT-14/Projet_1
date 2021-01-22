9e4d50670b771a1050b1593c0dbd391ed5366512
Basile <basile.botebol@gmail.com>
2021-01-22 09:40:03 +0100

    changes in the script

--- appRegistration.sh
+++ appRegistration.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo "API_KEY=" > app.env
 API_KEY=$(curl -X POST "http://localhost:8080/applications" -H "Content-Type: application/json" -d "{\"name\":\"application_project1\"}")
-echo "$API_KEY" >> app.env
+printf %s "API_KEY=$API_KEY" > app.env
 
 
 curl -X POST "http://localhost:8080/badges" -H "X-API-KEY: ${API_KEY}" -H "Content-Type: application/json" -d "{\"name\":\"badgeFirstDownVote\"}"
@@ -12,7 +11,7 @@ curl -X POST "http://localhost:8080/badges" -H "X-API-KEY: ${API_KEY}" -H "Conte
 curl -X POST "http://localhost:8080/scoreScales" -H "X-API-KEY: ${API_KEY}" -H "Content-Type: application/json" -d "{\"name\":\"interactionScore\"}"
 curl -X POST "http://localhost:8080/scoreScales" -H "X-API-KEY: ${API_KEY}" -H "Content-Type: application/json" -d "{\"name\":\"questionScore\"}"
 
-curl -X POST "http://localhost:8080/rules" -H "X-API-KEY: ${apiKey}" -H "Content-Type: application/json" -d "{\"badgeName\":\"badgeFirstDownVote\",\"description\":\"grant a badge on the first down vote and increment the interaction score\",\"eventName\":\"eventDownVote\",\"name\":\"ruleDownVote\",\"scoreDelta\":1,\"scoreScaleName\":\"interactionScore\"}"
-curl -X POST "http://localhost:8080/rules" -H "X-API-KEY: ${apiKey}" -H "Content-Type: application/json" -d "{\"badgeName\":\"badgeFirstQuestion\",\"description\":\"grant a badge on the first question and increment the question score\",\"eventName\":\"eventQuestion\",\"name\":\"ruleQuestion\",\"scoreDelta\":1,\"scoreScaleName\":\"questionScore\"}"
-curl -X POST "http://localhost:8080/rules" -H "X-API-KEY: ${apiKey}" -H "Content-Type: application/json" -d "{\"badgeName\":\"badgeFirstUpVote\",\"description\":\"grant a badge on the first up vote and increment the interaction score\",\"eventName\":\"eventUpVote\",\"name\":\"ruleUpVote\",\"scoreDelta\":1,\"scoreScaleName\":\"interactionScore\"}"
+curl -X POST "http://localhost:8080/rules" -H "X-API-KEY: ${API_KEY}" -H "Content-Type: application/json" -d "{\"badgeName\":\"badgeFirstDownVote\",\"description\":\"grant a badge on the first down vote and increment the interaction score\",\"eventName\":\"eventDownVote\",\"name\":\"ruleDownVote\",\"scoreDelta\":1,\"scoreScaleName\":\"interactionScore\"}"
+curl -X POST "http://localhost:8080/rules" -H "X-API-KEY: ${API_KEY}" -H "Content-Type: application/json" -d "{\"badgeName\":\"badgeFirstQuestion\",\"description\":\"grant a badge on the first question and increment the question score\",\"eventName\":\"eventQuestion\",\"name\":\"ruleQuestion\",\"scoreDelta\":1,\"scoreScaleName\":\"questionScore\"}"
+curl -X POST "http://localhost:8080/rules" -H "X-API-KEY: ${API_KEY}" -H "Content-Type: application/json" -d "{\"badgeName\":\"badgeFirstUpVote\",\"description\":\"grant a badge on the first up vote and increment the interaction score\",\"eventName\":\"eventUpVote\",\"name\":\"ruleUpVote\",\"scoreDelta\":1,\"scoreScaleName\":\"interactionScore\"}"
 
