42542bfa8ae0027297c0554aa424a9ec7496da59
ClaudeAlves <claudeuw@hotmail.com>
2021-01-22 12:20:20 +0100

    meilleure gestion de l'affichage des badges/scores

--- src/main/java/ch/heigvd/amt/ui/web/profile/ProfileEndPoint.java
+++ src/main/java/ch/heigvd/amt/ui/web/profile/ProfileEndPoint.java
@@ -55,7 +55,7 @@ public class ProfileEndPoint extends HttpServlet {
         StringBuilder stringBuilder = new StringBuilder();
         for(Badge badge : badges) {
             stringBuilder.append(badge.getName());
-            stringBuilder.append("\n");
+            stringBuilder.append("<\br>");
         }
         return stringBuilder.toString();
     }
@@ -63,10 +63,10 @@ public class ProfileEndPoint extends HttpServlet {
         StringBuilder stringBuilder = new StringBuilder();
         for(UserScore score : scores) {
             stringBuilder.append(score.getScore().getName());
-            stringBuilder.append("\n");
-            stringBuilder.append("\t");
+            stringBuilder.append("<br/>");
+            stringBuilder.append("&emsp;");
             stringBuilder.append(score.getScoreValue());
-            stringBuilder.append("\n");
+            stringBuilder.append("<br/>");
         }
         return stringBuilder.toString();
     }
